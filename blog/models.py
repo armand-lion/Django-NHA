cc411a044f29f438f82ae79fb09ee65dc3c26e3a
armand-lion <armand.lion.nl@gmail.com>
2026-09-12 15:16:18 +0200

    toevoeging nieuwe database tabel:voetbalspelers

--- blog/models.py
+++ blog/models.py
@@ -18,3 +18,17 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+class Voetbalspelers(models.Model):
+    spelersnaam = models.CharField(max_length=30)
+    voetbalclub = models.CharField(max_length=30)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(default=timezone.now)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(blank=True, null=True)
+    
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.spelersnaam
